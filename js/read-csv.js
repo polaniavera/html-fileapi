bb12bccb6f4338a4e91515fbdb0e1f67ceff6646
Cristhian Polania <polaniavera@gmail.com>
2018-03-27 19:17:50 +1100

    Add Json format

--- js/read-csv.js
+++ js/read-csv.js
@@ -22,14 +22,17 @@ function loadHandler(event) {
 	processData(csv);             
 }
 
-function processData(csv) {
-    var allTextLines = csv.split(/\r\n|\n/);
+function processData(csv) {	
+	var allTextLines = csv.split(/\r\n|\n/);
     var lines = [];
     while (allTextLines.length) {
-        lines.push(allTextLines.shift().split(','));
-    }
-	console.log(lines);
-	drawOutput(lines);
+		lines.push(allTextLines.shift().split(','));
+	}
+
+	var arrMod = formatArray(lines);
+	var myJSON = JSON.stringify(arrMod);
+	document.getElementById("output").innerHTML += myJSON;
+	//drawOutput(myJSON);
 }
 
 function errorHandler(evt) {
@@ -49,5 +52,64 @@ function drawOutput(lines){
 			firstNameCell.appendChild(document.createTextNode(lines[i][j]));
 		}
 	}
-	document.getElementById("output").appendChild(table);
+	document.getElementById("output").appendChild(lines);
+}
+
+//***************************************************************************************** */
+//***************************************************************************************** */
+
+function formatArray(arr){
+	var objectMain = [];
+	//Get current date and time
+	var date = formatDate();
+	var time = formatTime();
+
+	for(i = 1; i < arr.length-1; i++){
+		var objectSingle = { };
+		objectSingle.Latitud = 9.99999;
+		objectSingle.Longitud = -74.44444;
+		objectSingle.TanqueConductor = parseFloat(arr[i][0]);
+		objectSingle.TanquePasajero = parseFloat(arr[i][1]);
+		objectSingle.BotonPanico = true;
+		objectSingle.Velocidad = parseFloat(arr[i][2]);
+		objectSingle.Fecha = date;
+		objectSingle.Hora = time;
+		objectSingle.IdUsuario = 1;
+		objectSingle.IdItem = 1;
+
+		objectMain.push(objectSingle);
+	}
+	return objectMain;
+}
+
+function formatDate(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+	if(dd<10) {
+		dd = '0'+dd
+	} 
+	if(mm<10) {
+		mm = '0'+mm
+	} 
+	today = yyyy + '-' + mm + '-' + dd;
+	return today.toString();
+}
+
+function formatTime(){
+	var hour = new Date();
+	var hh = hour.getHours();  
+    var min = hour.getMinutes();
+	var ss = hour.getSeconds();
+	hour = hh + ':' + min + ':' + ss;
+	return hour.toString();
+}
+
+function newTime(oldTime){
+	var hh = oldTime.getHours();  
+    var min = oldTime.getMinutes();
+	var ss = oldTime.getSeconds();
+	oldTime = hh + ':' + min + ':' + ss;
+	return hour.toString();
 }
